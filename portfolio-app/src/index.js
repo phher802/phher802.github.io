07aaeb8cf0807a78c350dbb812ee03eeebdbf339
phher802 <pher024@gmail.com>
2021-08-02 00:46:10 -0500

    Add @material-ui/icons package

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-window.React1 = require('react');
 
 ReactDOM.render(<React.StrictMode>
     <App />
