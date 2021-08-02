1446159b3f890819c1cdfa5840604edba5f6a366
phher802 <pher024@gmail.com>
2021-08-02 00:30:44 -0500

    Add Home.jsx and update images file route path

---
+++ portfolio-app/src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Home() {
+
+
+    return ( 
+        <div>
+            <div className="top-container">
+                <h1>I'm Pa</h1>
+                <h3>a software developer</h3>
+                <img className="cloud-1 clouds-animate-1" src="public/images/cloud1.png" alt="cloud img" />
+                <img className="cloud-2 clouds-animate-2" src="public/images/cloud1.png" alt="cloud img" />
+                <img className="cloud-3 clouds-animate-3" src="public/images/cloud1.png" alt="cloud img" />
+                <img className="cloud-4 clouds-animate-4" src="public/images/cloud1.png" alt="cloud img" />
+                <img className="cloud-5 clouds-animate-5" src="public/images/cloud1.png" alt="cloud img" />
+                <img className="island" src="images/whaleIslandVacay.gif" alt="whale island cartoon" />
+            </div>
+            <div className="middle-container">
+                <div className="self-box">
+                    <img className="self" src="public/images/stackedPolaroidWithSelfie.svg" alt="Pa" />
+                </div>
+
+                <div className="hello">
+                    <h1 className="font1">Hello.</h1>
+                    <h2 className="my-interests">I'm a lover of Pho, Bloody Marys, and Korean Fried Chicken.</h2>
+                </div>
+            </div>
+            
+        </div>
+ 
+    );
+}
+
+export default Home;
